8e3133ddb712c67943e224e37b2393e3adeb6fab
Cassius <cassius.reynolds.dev@gmail.com>
2025-01-10 15:31:19 -0800

    added services dir to communicate with backend

---
+++ src/services/authService.js
@@ -0,0 +1,90 @@
+import axios from "axios";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+/////////////////////
+// ? POST | User Signup Function
+/////////////////////
+export async function signup(formData) {
+  try {
+    const response = await axios.post(`${BASE_URL}/auth/register`, formData);
+
+    const data = response.data;
+    if (data.err) {
+      throw new Error(data.err);
+    }
+
+    if (data.token) {
+      // store the token! in localstorage
+      localStorage.setItem("token", data.token);
+      const user = JSON.parse(atob(data.token.split(".")[1]));
+      console.log(user, " <- user in signup!");
+      return user.user;
+    }
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+}
+
+/////////////////////
+// User login Function
+/////////////////////
+export async function login(userCredentials) {
+  try {
+    const response = await axios.post(
+      `${BASE_URL}/auth/login`,
+      userCredentials,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const data = response.data;
+    if (data.error) {
+      throw new Error(data.error);
+    }
+
+    if (data.token) {
+      // store the token! in localstorage
+      localStorage.setItem("token", data.token);
+      const user = JSON.parse(atob(data.token.split(".")[1]));
+      return user.user;
+    }
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
+
+/////////////////////
+// Get User Function
+/////////////////////
+export function getUser() {
+  const token = localStorage.getItem("token");
+  const user = JSON.parse(atob(token.split(".")[1]));
+  return user.user;
+}
+
+/////////////////////
+// Refresh Token
+/////////////////////
+export const refreshToken = async () => {
+  try {
+    const response = await axios.post(
+      `${BASE_URL}/refresh-token`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+    const { token } = response.data;
+    localStorage.setItem("token", token);
+    return token;
+  } catch (err) {
+    console.error("Error refreshing token:", err);
+  }
+};
---
+++ src/services/formService.js
@@ -0,0 +1,127 @@
+import axios from "axios";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+const SPOTLIGHT_BASE_URL = BASE_URL + "/spotlights";
+const TESTIMONIALS_BASE_URL = BASE_URL + "/testimonials";
+
+///////////////////////////
+// Athlete Spotlights
+///////////////////////////
+export const postAddSpotlight = async (formData) => {
+  try {
+    const response = await axios.post(`${SPOTLIGHT_BASE_URL}`, formData);
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    console.log("Failed to create a new athlete spotlight.");
+    throw err;
+  }
+};
+
+export const putUpdateSpotlight = async (formData, userId) => {
+  try {
+    const response = await axios.put(
+      `${SPOTLIGHT_BASE_URL}/${userId}`,
+      formData
+    );
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    console.log(`Failed to update athlete spotlight for user ID: ${userId}`);
+    throw err;
+  }
+};
+
+export const deleteRemoveSpotlight = async (userId) => {
+  try {
+    const response = await axios.delete(`${SPOTLIGHT_BASE_URL}/${userId}`);
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    console.log(`Failed to remove athlete spotlight for user ID: ${userId}`);
+    throw err;
+  }
+};
+
+export const getAllSpotlights = async () => {
+  try {
+    const response = await axios.get(`${SPOTLIGHT_BASE_URL}`);
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    console.log("Failed to fetch all athlete spotlights.");
+    throw err;
+  }
+};
+
+export const getApprovedSpotlights = async () => {
+  try {
+    const response = await axios.get(`${SPOTLIGHT_BASE_URL}/approved`);
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    console.log("Failed to fetch approved athlete spotlights.");
+    throw err;
+  }
+};
+
+///////////////////////////
+// Testimonials
+///////////////////////////
+
+export const postAddTestimonial = async (formData) => {
+  try {
+    const response = await axios.post(`${TESTIMONIALS_BASE_URL}`, formData);
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    console.log("Failed to create a new athlete Testimonial.");
+    throw err;
+  }
+};
+
+export const putUpdateTestimonial = async (formData, userId) => {
+  try {
+    const response = await axios.put(
+      `${TESTIMONIALS_BASE_URL}/${userId}`,
+      formData
+    );
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    console.log(`Failed to update athlete Testimonial for user ID: ${userId}`);
+    throw err;
+  }
+};
+
+export const deleteRemoveTestimonial = async (userId) => {
+  try {
+    const response = await axios.delete(`${TESTIMONIALS_BASE_URL}/${userId}`);
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    console.log(`Failed to remove athlete Testimonial for user ID: ${userId}`);
+    throw err;
+  }
+};
+
+export const getAllTestimonials = async () => {
+  try {
+    const response = await axios.get(`${TESTIMONIALS_BASE_URL}`);
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    console.log("Failed to fetch all athlete Testimonials.");
+    throw err;
+  }
+};
+
+export const getApprovedTestimonials = async () => {
+  try {
+    const response = await axios.get(`${TESTIMONIALS_BASE_URL}/approved`);
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    console.log("Failed to fetch approved athlete spotlights.");
+    throw err;
+  }
+};
