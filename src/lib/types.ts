6574afd02be0c7c94597fa1f9ab824c61e13c5fa
Cassius <cassius.reynolds.dev@gmail.com>
2025-02-12 22:39:32 -0800

    added eslint disable for any type error in types.ts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -173,6 +173,7 @@ export interface GlobalContextType {
   ) => void;
   handleInputChange: (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     setState: React.Dispatch<React.SetStateAction<any>>
   ) => void;
   handleNextFormStep: () => void;
