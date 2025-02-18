866bd3f665cf5c0302ecbc9ca74252d60c4fd9dc
Cassius <cassius.reynolds.dev@gmail.com>
2025-02-18 03:11:38 -0800

    fixed share exp page where container was not showing entirely on mobile dur to height setting. Also made footer zindex negative to ensrue that the main content is always showing incase an edge case for speciial bg styling causes the content to overlap with the footer. Also added auror to prompt sign in component

--- src/components/Auth/PromptLoginOrRegister.tsx
+++ src/components/Auth/PromptLoginOrRegister.tsx
@@ -1,42 +1,46 @@
 import { Link } from "react-router-dom";
+import { AuroraContainer } from "../Aurora";
 
 const PromptLoginOrRegister = () => {
   return (
-    <div className="w-full md:w-1/2 lg:w-1/3 mx-auto p-4 bg-neutral-900 rounded-lg shadow-md mt-20 text-center">
-      <h2 className="text-2xl font-semibold text-white mb-4">
-        Create Athlete Spotlight
-      </h2>
+    <AuroraContainer>
+      <div className="w-full md:w-1/2 lg:w-1/3 mx-auto p-4 bg-neutral-900 rounded-lg shadow-md mt-20 text-center">
+        <h2 className="text-2xl font-semibold text-white mb-4">
+          Create Athlete Spotlight
+        </h2>
 
-      <p className="text-sm text-gray-200 mb-6">
-        To create an Athlete Spotlight, you need to either sign in or create an
-        account. Your post will be subject to review before being published.
-        <br />
-        <br />
-        If you create an account, you will be able to edit your spotlight in the
-        future. Without an account, you will not be able to make any changes
-        after submission!
-        <br />
-        <br />
-        <span className="font-semibold text-white">
-          Email is required for sign up, but your phone number is optional.
-        </span>
-      </p>
+        <p className="text-sm text-gray-200 mb-6">
+          To create an Athlete Spotlight, you need to either sign in or create
+          an account. Your post will be subject to review before being
+          published.
+          <br />
+          <br />
+          If you create an account, you will be able to edit your spotlight in
+          the future. Without an account, you will not be able to make any
+          changes after submission!
+          <br />
+          <br />
+          <span className="font-semibold text-white">
+            Email is required for sign up, but your phone number is optional.
+          </span>
+        </p>
 
-      <div className="flex justify-center gap-4">
-        <Link
-          to="/auth/login"
-          className="px-6 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 transition-colors"
-        >
-          Login
-        </Link>
-        <Link
-          to="/auth/register"
-          className="px-6 py-2 text-white bg-green-600 rounded-md hover:bg-green-700 transition-colors"
-        >
-          Register
-        </Link>
+        <div className="flex justify-center gap-4">
+          <Link
+            to="/auth/login"
+            className="px-6 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 transition-colors"
+          >
+            Login
+          </Link>
+          <Link
+            to="/auth/register"
+            className="px-6 py-2 text-white bg-green-600 rounded-md hover:bg-green-700 transition-colors"
+          >
+            Register
+          </Link>
+        </div>
       </div>
-    </div>
+    </AuroraContainer>
   );
 };
 export default PromptLoginOrRegister;
--- src/components/ShareExp.tsx
+++ src/components/ShareExp.tsx
@@ -1,4 +1,4 @@
-import { AuroraContainer} from "./Aurora";
+import { AuroraContainer } from "./Aurora";
 import FormDescriptionCard from "./FormDescriptionCard";
 
 const ShareExp = () => {
@@ -11,32 +11,32 @@ const ShareExp = () => {
           style={{
             backgroundImage:
               "url('https://clients-cr.s3.us-west-1.amazonaws.com/a2a/images/spotlights-home-aftergame.jpg')",
-              marginTop:'-1rem'
+            marginTop: "-1rem",
           }}
         >
           {/* Dark overlay */}
           <div className="absolute inset-0 bg-black opacity-80 w-full"></div>{" "}
-          <div className="relative z-10 flex flex-col items-center justify-end gap-12 h-full text-center text-white px-4 sm:px-6 lg:px-8">
+          <div className="relative z-10 pt-20 md:pt-32  flex flex-col items-center justify-end gap-12 text-center text-white px-4 sm:px-6 lg:px-8">
             <div>
-              <h1 className="text-5xl font-bold leading-tight sm:text-6xl lg:text-7xl mb-6">
+              <h1 className="text-4xl font-bold leading-tight sm:text-5xl lg:text-6xl mb-6 md:mb-16">
                 Empowering Athletes. Inspiring Excellence.
               </h1>
-              <p className="text-lg sm:text-xl lg:text-2xl mb-6">
+              <p className="text-lg sm:text-xl lg:text-2xl mb-6 max-w-5xl mx-auto">
                 We appreciate every athlete or parent who would be willing to
                 either fill out an Athlete Spotlight story or leave a genuine
                 testimonial/review to support our community and continue to
                 allow us to chase greatness.
               </p>
               <br />
-              <p className="text-lg sm:text-xl lg:text-2xl mb-6">
+              <p className="text-lg lg:text-xl mb-6 max-w-5xl mx-auto">
                 {" "}
                 Please create an account to have your spotlight or testimonial
                 elgible to appear on the website.{" "}
               </p>
             </div>{" "}
+            {/* Cards Section */}
             <div>
-              {/* Cards Section */}
-              <div className="py-16  text-white">
+              <div className="pb-12 md:pb-16  text-white">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                   <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
                     {/* Athlete Spotlight Card */}
